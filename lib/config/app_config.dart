4730ecd4a2ff77267c5d9738956a15138d32ba98
mhmadhfz <apiss2272@gmail.com>
2026-04-02 20:17:03 +0800

    Update app_config.dart

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,11 +1,11 @@
 class AppConfig {
   // 🔥 Backend API
   static const String baseUrl =
-      "http://192.168.0.105/facetrack_backend/public/api";
+      "http://192.168.0.107/facetrack_backend/public/api";
 
   // 🔥 Base backend root (without /api)
   static const String backendRoot =
-      "http://192.168.0.105/facetrack_backend/public";
+      "http://192.168.0.107/facetrack_backend/public";
 
   // 🔥 Storage URL
   static String storageUrl(String path) {
